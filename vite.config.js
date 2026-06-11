8b791eb84df9464440257e8494e97e848ba8999b
momoelo1 <momoelo1@hotmail.com>
2026-06-11 18:55:28 +0200

    fix base path and api url

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  
-  base: process.env.NODE_ENV === 'production' ? '/kozmo-brand-frontend/' : '/',
- 
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/kozmo-brand-frontend/' : '/',
+
   plugins: [react()],
   server: {
     host: '0.0.0.0',
