737d4b30c6874f842b4bc0dae4924cdae613688c
momoelo1 <momoelo1@hotmail.com>
2026-06-11 18:58:31 +0200

    fix base path and api url

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,35 @@
 /* eslint-disable no-undef */
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/kozmo-brand-frontend/' : '/',
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === "production" ? "/kozmo-brand-frontend/" : "/",
 
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        secure: false,
+    plugins: [react()],
+    server: {
+      host: "0.0.0.0",
+      proxy: {
+        "/api": {
+          target: "http://localhost:3001",
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
-  build: {
-    target: ['es2015', 'safari11', 'firefox60'],
-    cssTarget: ['safari11', 'firefox60'],
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          redux: ['@reduxjs/toolkit', 'react-redux'],
-          ui: ['@fortawesome/react-fontawesome'],
-        }
-      }
-    }
-  }
-})
+    build: {
+      target: ["es2015", "safari11", "firefox60"],
+      cssTarget: ["safari11", "firefox60"],
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            vendor: ["react", "react-dom", "react-router-dom"],
+            redux: ["@reduxjs/toolkit", "react-redux"],
+            ui: ["@fortawesome/react-fontawesome"],
+          },
+        },
+      },
+    },
+  };
+});
